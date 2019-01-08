5190e3d717b56a1b603b5de81d41daad90aa716a
alabra <alllexe@mail.ru>
2019-01-08 13:04:19 +0300

    1.3. JavaDoc. Code convention[#101944]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,7 +1,17 @@
 package ru.job4j.calculate;
 
+/**
+	Класс Calculate для вычисления арифметических операций + - * / ^.
+	@author Alexandr Abramov
+	@since 08.01.2019
+	@version 1
+*/
 public class Calculate {
 	
+	/**
+		Конструктор, вывод строки в консоль
+		@param args - массив строк, параметры запуска
+	*/
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 	}
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Alexandr Abramov (alllexe@ymail.ru)
+* @version $Id$
+* @since 0.1
+*/
+package ru.job4j.calculate;
