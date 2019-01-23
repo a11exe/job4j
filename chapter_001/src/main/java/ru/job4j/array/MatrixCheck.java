8a50426af88d7f8d2e1c960c8d575cbfdd33980c
alabra <alllexe@mail.ru>
2019-01-23 10:41:51 +0300

    fix MatrixCheck add all checks in one loop

--- chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -15,27 +15,31 @@ public class MatrixCheck {
      * @return по диагонали одинаковые значения true или false.
      */
     public boolean mono(boolean[][] data) {
+
         boolean result = true;
+
         if (data.length > 1) {
-            // check from left to right
+
             boolean checked = data[0][0];
-            for (int i = 1; i < data.length; i++) {
-                if (checked != data[i][i]) {
-                    result = false;
-                    break;
-                }
-            }
-            // check from right to left
-            if (result) {
-                checked = data[0][data.length - 1];
+
+            if (checked != data[data.length - 1][0]) {
+                result = false;
+            } else {
                 for (int i = 1; i < data.length; i++) {
-                    if (checked != data[i][data.length - 1 - i]) {
+                    // check from left to right
+                    if (checked != data[i][i]) {
+                        result = false;
+                        break;
+                    }
+                    // check from right to left
+                    if (checked != data[data.length - 1 - i][i]) {
                         result = false;
                         break;
                     }
                 }
             }
         }
+
         return result;
     }
 
