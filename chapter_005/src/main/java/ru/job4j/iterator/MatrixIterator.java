38d3e36bd6ebc7b3b89afb187a2df6dc85b6d6db
alabra <alllexe@mail.ru>
2019-03-19 20:50:39 +0300

    Итератор для двухмерного массива int[][][#123319]

---
+++ chapter_005/src/main/java/ru/job4j/iterator/MatrixIterator.java
@@ -0,0 +1,51 @@
+package ru.job4j.iterator;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * Итератор двумерного массива
+ * @author Alexander Abramov (alllexe@mail.ru)
+ * @version 1
+ * @since 19.03.2019
+ */
+public class MatrixIterator implements Iterator<Integer> {
+
+    private final int[][] array;
+    private int x = 0;
+    private int y = 0;
+    private long count = 0;
+    private final long size;
+
+    public MatrixIterator(int[][] array) {
+        this.array = array;
+        this.size = Arrays.stream(array).flatMapToInt(Arrays::stream).count();
+    }
+
+    @Override
+    public boolean hasNext() {
+        return count < this.size;
+    }
+
+    @Override
+    public Integer next() {
+        int next;
+        try {
+            next = this.array[this.x][this.y];
+        } catch (ArrayIndexOutOfBoundsException e) {
+            throw new NoSuchElementException("no next element");
+        }
+        if (this.array[this.x].length - 1 > this.y) {
+            this.y++;
+        } else {
+            if (this.array.length - 1 > this.x) {
+                this.x++;
+                this.y = 0;
+            }
+        }
+        count++;
+
+        return next;
+    }
+}
---
+++ chapter_005/src/test/java/ru/job4j/iterator/JaggedArrayIteratorTest.java
@@ -0,0 +1,50 @@
+package ru.job4j.iterator;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Iterator;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class JaggedArrayIteratorTest {
+
+    private Iterator<Integer> it;
+
+    @Before
+    public void setUp() {
+        it = new MatrixIterator(new int[][]{{1}, {3, 4}, {7}});
+    }
+
+    @Test
+    public void testsThatNextMethodDoesntDependsOnPriorHasNextInvocation() {
+        assertThat(it.next(), is(1));
+        assertThat(it.next(), is(3));
+        assertThat(it.next(), is(4));
+        assertThat(it.next(), is(7));
+    }
+
+    @Test
+    public void sequentialHasNextInvocationDoesntAffectRetrievalOrder() {
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(1));
+        assertThat(it.next(), is(3));
+        assertThat(it.next(), is(4));
+        assertThat(it.next(), is(7));
+    }
+
+    @Test
+    public void hasNextNextSequentialInvocation() {
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(1));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(3));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(4));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(7));
+        assertThat(it.hasNext(), is(false));
+    }
+}
