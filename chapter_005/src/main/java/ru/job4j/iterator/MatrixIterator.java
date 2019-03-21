74a475a655c8cb6753cd888ecc1316ba73f53098
alabra <alllexe@mail.ru>
2019-03-21 10:16:32 +0300

    hasNext использовал в методе next. упростил код.

--- chapter_005/src/main/java/ru/job4j/iterator/MatrixIterator.java
+++ chapter_005/src/main/java/ru/job4j/iterator/MatrixIterator.java
@@ -1,6 +1,5 @@
 package ru.job4j.iterator;
 
-import java.util.Arrays;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
@@ -15,37 +14,33 @@ public class MatrixIterator implements Iterator<Integer> {
     private final int[][] array;
     private int x = 0;
     private int y = 0;
-    private long count = 0;
-    private final long size;
 
     public MatrixIterator(int[][] array) {
         this.array = array;
-        this.size = Arrays.stream(array).flatMapToInt(Arrays::stream).count();
     }
 
     @Override
     public boolean hasNext() {
-        return count < this.size;
+        return (x < this.array.length);
     }
 
     @Override
     public Integer next() {
         int next;
-        if (x > this.array.length -1) {
+        if (!hasNext()) {
             throw new NoSuchElementException("no next element");
         }
-        next = this.array[this.x][this.y];
-
-        if (this.array[this.x].length - 1 > this.y) {
-            this.y++;
-        } else {
-            if (this.array.length - 1 > this.x) {
-                this.x++;
-                this.y = 0;
-            }
-        }
-        count++;
-
+        next = this.array[x][y];
+        moveCursor();
         return next;
+    }
+
+    private void moveCursor() {
+        if (this.array[x].length - 1 > y) {
+            y++;
+        } else {
+            x++;
+            y = 0;
+        }
     }
 }
