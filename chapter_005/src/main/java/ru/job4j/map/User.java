f5ed9664cf745bc24a27062920b7b3ee3d98ea9e
alabra <alllexe@mail.ru>
2019-03-28 14:29:56 +0300

    Создать модель User[#123328]

---
+++ chapter_005/src/main/java/ru/job4j/map/User.java
@@ -0,0 +1,17 @@
+package ru.job4j.map;
+
+import java.util.Calendar;
+
+/**
+ * Пользователь
+ * @author Alexander Abramov (alllexe@mail.ru)
+ * @version 1
+ * @since 28.03.2019
+ */
+public class User {
+
+    private String name;
+    private int children;
+    private Calendar birthday;
+
+}
