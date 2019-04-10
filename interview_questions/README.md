ff6cbfd94f61d1173ca18f49b133e6df17668b5f
alabra <alllexe@mail.ru>
2019-04-10 13:48:37 +0300

    Collections pro questions

--- interview_questions/README.md
+++ interview_questions/README.md
@@ -3,8 +3,8 @@
 
 + [Java Core]() ![icon][done]
 + [OOP]() ![icon][done]
-+ [Collection lite]() ![icon][done]
-+ [FP, Lambda, Stream API](FPLambdaStreamAPI.md#fp-labmda-stream-api)
-
++ [Collection lite](CollectionLite.md#collections-light) ![icon][done]
++ [FP, Lambda, Stream API](FPLambdaStreamAPI.md#fp-labmda-stream-api)![icon][done]
++ [Collection pro](CollectionPro.md#collections-pro)
 
 [done]:done.png
---
+++ utils/src/main/java/ru.job4j/MDUtils.java
@@ -0,0 +1,50 @@
+package ru.job4j;
+
+import java.util.Scanner;
+
+/**
+ * Преобразует строку в заголовок со ссылко для файлов
+ * README.md в github
+ *
+ * @author Alexander Abramov (alllexe@mail.ru)
+ * @version 1
+ * @since 10.04.2019
+ */
+public class MDUtils {
+
+    public String getMenuItem(String str) {
+        str = str.replaceAll("#", "");
+        str = str.replaceAll("\\(", "");
+        str = str.replaceAll("\\)", "");
+        str = str.replaceAll("\\[", "");
+        str = str.replaceAll("\\]", "");
+        str = str.replaceAll("\\+", "");
+        str = str.trim();
+
+        String question = str;
+        question = "+ [" + question + "]";
+
+        str = str.replaceAll("\\.", "");
+        str = str.replaceAll("\\,", "");
+        str = str.replaceAll("\\?", "");
+        str = str.replaceAll(" ", "-");
+
+        return question + "(#" + str + ")";
+    }
+
+    public static void main(String[] args) {
+
+        String str;
+        MDUtils mdUtils = new MDUtils();
+        Scanner input = new Scanner(System.in);
+        while (input.hasNextLine()) {
+            str = input.nextLine();
+            if (str.isEmpty()) {
+                break;
+            }
+            System.out.println(mdUtils.getMenuItem(str));
+        }
+    }
+
+
+}
