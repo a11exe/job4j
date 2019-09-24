c8b3741e309512746503cf079bcddaccd4e36b6d
alabra <alllexe@mail.ru>
2019-09-24 15:04:19 +0300

    Junit тест для блокирующей очереди.[#176236]

---
+++ chapter_010/src/main/java/ru/job4j/queue/SimpleBlockingQueue.java
@@ -0,0 +1,48 @@
+package ru.job4j.queue;
+
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
+import java.util.LinkedList;
+import java.util.Queue;
+/**
+ * @author Alexander Abramov (alllexe@mail.ru)
+ * @version 1
+ * @since 24.09.2019
+ */
+@ThreadSafe
+public class SimpleBlockingQueue<T> {
+    @GuardedBy("this")
+    private final Queue<T> queue = new LinkedList<>();
+    private final int size;
+
+    public SimpleBlockingQueue(int size) {
+        this.size = size;
+    }
+
+    public synchronized void offer(T value) throws InterruptedException {
+        while (queue.size() == this.size) {
+            wait();
+        }
+        queue.offer(value);
+        System.out.println(String.format("Producer set: %s actual size: %s", value, queue.size()));
+        notify();
+
+    }
+
+    public synchronized T poll() throws InterruptedException {
+        T t = null;
+        while (queue.size() == 0) {
+            wait();
+        }
+        t = queue.poll();
+        System.out.println(String.format("Consumer get: %s actual size: %s", t, queue.size()));
+        notify();
+
+        return t;
+    }
+
+    public synchronized boolean isEmpty() {
+        return queue.isEmpty();
+    }
+}
---
+++ chapter_010/src/test/java/ru/job4j/queue/SimpleBlockingQueueTest.java
@@ -0,0 +1,58 @@
+package ru.job4j.queue;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+/**
+ * @author Alexander Abramov (alllexe@mail.ru)
+ * @version 1
+ * @since 24.09.2019
+ */
+public class SimpleBlockingQueueTest {
+
+    @Test
+    public void testSimpleBlockingQueue() throws InterruptedException {
+
+        final CopyOnWriteArrayList<Integer> buffer = new CopyOnWriteArrayList<>();
+        final SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(5);
+        Thread producer = new Thread(
+                () -> {
+                    for (int i = 0; i < 5; i++) {
+                        try {
+                            queue.offer(i);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
+        producer.start();
+        Thread consumer = new Thread(
+                () -> {
+                    while (!queue.isEmpty() || !Thread.currentThread().isInterrupted()) {
+                        try {
+                            buffer.add(queue.poll());
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
+        consumer.start();
+        producer.join();
+        consumer.interrupt();
+        consumer.join();
+
+        assertThat(buffer, is(Arrays.asList(0, 1, 2, 3, 4)));
+
+    }
+
+
+}
