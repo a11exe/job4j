e260cee4f9b6be733c82a0b163d9563e4b6a0566
alabra <alllexe@mail.ru>
2019-10-01 16:30:11 +0300

    Создать web проект[#176207]

---
+++ chapter_011/src/main/java/ru/job4j/servlets/EchoServlet.java
@@ -0,0 +1,24 @@
+package ru.job4j.servlets;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * @author Alexander Abramov (alllexe@mail.ru)
+ * @version 1
+ * @since 01.10.2019
+ */
+public class EchoServlet extends HttpServlet {
+
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        res.setContentType("text/html");
+        PrintWriter writer = new PrintWriter(res.getOutputStream());
+        writer.append("hello world");
+        writer.flush();
+    }
+
+}
